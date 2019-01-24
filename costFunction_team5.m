6593ce9154e42095665b9e9c3ab33c142eba27bf
mahabdh <44704171+mahabdh@users.noreply.github.com>
2019-01-24 14:39:29 +0100

    Delete costFunction_team5.m

--- costFunction_team5.m
+++
@@ -1,9 +0,0 @@
-function [J, grad] = costFunction_team5(theta, X, y)
-m = length(y); 
-J = 0;
-grad = zeros(size(theta));
-h = sigmoid_team5(X*theta);
-J = ( (-y)' *log(h)-(1-y)' * log(1-h))/m;
-grad = (X'*(h - y))/m;
-end
-
